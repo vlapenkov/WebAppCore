26662ad57ed675d3425b5dbe4fc56daeaccf4948
Лапенков Владимир <vlapenkov@ya.ru>
2018-10-25 11:03:56 +0300

    custom page derived from PageModel with it's own logic

---
+++ WebAppCore/Pages/CustomPage.cshtml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebAppCore.Data;
+
+namespace WebAppCore
+{
+    public class CustomPageModel : PageModel
+    {
+        private readonly ApplicationDbContext _appDbContext;
+
+        public CustomPageModel(ApplicationDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+       
+        public string Message { get; private set; } = "PageModel in C#";
+
+        public string FirstPersonName { get; set; }
+
+        public void OnGet(string param1, int? page)
+        {
+            Message += $" Server time is { DateTime.Now } ,param1 is:{param1} , page is : {page}";
+
+            FirstPersonName = _appDbContext.Persons.First()?.Name;
+        }
+    }
+}
